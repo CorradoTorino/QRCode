e383c99b92c3c9f743647492103e2433807281c2
Corrado <corrado.torino@gmail.com>
2020-03-07 23:31:41 +0100

    visualize QR code

--- QRCode/MainWindow.xaml.cs
+++ QRCode/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Windows;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace QRCode
+namespace QRCodeSample
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -16,15 +18,67 @@ namespace QRCode
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private static Bitmap CreateQrCode()
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("The text which should be encoded.", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+           return qrCode.GetGraphic(20);
+        }
+
+        private static BitmapImage GetMoon()
         {
             string selectedFileName = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "Moon.jpg");
-            FileNameLabel.Content = selectedFileName;
+            //FileNameLabel.Content = selectedFileName;
             BitmapImage bitmap = new BitmapImage();
             bitmap.BeginInit();
             bitmap.UriSource = new Uri(selectedFileName);
             bitmap.EndInit();
-            ImageViewer1.Source = bitmap;
+            return bitmap;
+        }
+
+        private static BitmapImage GetQR()
+        {
+            return BitmapToBitmapImage(CreateQrCode());
+        }
+
+        private static Bitmap BitmapImageToBitmap(BitmapImage bitmapImage)
+        {
+            // BitmapImage bitmapImage = new BitmapImage(new Uri("../Images/test.png", UriKind.Relative));
+
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+                return new Bitmap(bitmap);
+            }
+        }
+
+        public static BitmapImage BitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //ImageViewer1.Source = GetMoon();
+            ImageViewer1.Source = GetQR();
         }
     }
 }
